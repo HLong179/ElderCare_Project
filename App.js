e25c94c11b587c63d3389b99e904e72d23d2fa4e
dhoaison <1512459@student.hcmus.edu.vn>
2019-03-05 11:32:24 +0700

    Add login screen

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Login from './src/components/Login';
+import AppNavigator from './src/components/appNavigator';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,14 +19,17 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+export default class App extends Component/*<Props>*/ {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+        {/* <Login></Login> */}
+        <AppNavigator></AppNavigator>
+        
       </View>
     );
   }
@@ -34,8 +39,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // alignItems: 'center',
+    backgroundColor: '#DCDCDC',
   },
   welcome: {
     fontSize: 20,
