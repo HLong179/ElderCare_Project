7b1a54e4539db54bab6f77697950b16283d83971
HaiLong168 <Hailong168us@gmail.com>
2019-04-15 22:10:59 +0700

    re Init code

--- App/Components/CommonButton/styled/StyledText.js
+++
@@ -1,9 +0,0 @@
-import { Text } from 'native-base';
-import styled from 'styled-components';
-
-const StyledText = styled(Text)`
-  color: black;
-  font-weight: bold;
-`;
-
-export default StyledText;
